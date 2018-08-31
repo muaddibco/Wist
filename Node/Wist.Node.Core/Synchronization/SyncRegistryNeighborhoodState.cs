672adea3d57afc2543d895922b66e3e83280ee43
Kirill Gandyl <muaddibco@gmail.com>
2018-09-01 01:56:09 +0300

    bug fix

--- Node/Wist.Node.Core/Synchronization/SyncRegistryNeighborhoodState.cs
+++ Node/Wist.Node.Core/Synchronization/SyncRegistryNeighborhoodState.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Wist.Core.Architecture;
+using Wist.Core.Architecture.Enums;
 using Wist.Core.States;
 
 namespace Wist.Node.Core.Synchronization
 {
+    [RegisterExtension(typeof(IState), Lifetime = LifetimeManagement.Singleton)]
     public class SyncRegistryNeighborhoodState : NeighborhoodStateBase, ISyncRegistryNeighborhoodState
     {
-        public static string NAME = "SyncRegistryNeighborhood";
-
-        public override string Name => NAME;
+        public override string Name => nameof(ISyncRegistryNeighborhoodState);
     }
 }
