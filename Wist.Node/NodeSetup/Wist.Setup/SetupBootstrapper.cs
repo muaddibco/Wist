c9cde15dcb60658747626a82cb934c9886de9f46
Kirill Gandyl <muaddibco@gmail.com>
2018-10-09 19:28:36 +0300

    Fixed setup

--- Wist.Node/NodeSetup/Wist.Setup/SetupBootstrapper.cs
+++ Wist.Node/NodeSetup/Wist.Setup/SetupBootstrapper.cs
@@ -1,4 +1,7 @@
 ﻿using CommonServiceLocator;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Threading;
 using Wist.Core.Architecture;
 using Wist.Core.PerformanceCounters;
@@ -7,8 +10,18 @@ namespace Wist.Setup
 {
     public class SetupBootstrapper : Bootstrapper
     {
+        private readonly string[] _catalogItems = new string[] { "Wist.Crypto.dll", "Chaos.NaCl.dll", "Wist.Network.dll", "Wist.Node.Core.dll" };
+
         public SetupBootstrapper(CancellationToken ct) : base(ct)
         {
+        }
+
+
+        protected override IEnumerable<string> EnumerateCatalogItems(string rootFolder)
+        {
+            return base.EnumerateCatalogItems(rootFolder)
+                .Concat(_catalogItems)
+                .Concat(Directory.EnumerateFiles(rootFolder, "Wist.BlockLattice.*.dll").Select(f => new FileInfo(f).Name));
         }
 
         protected override void RunInitializers()
